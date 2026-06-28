88ec5e99c19a3646471fb427275deeedc1778be2
qiin2333 <414382190@qq.com>
2026-06-28 12:49:11 +0800

    refactor(gui): auto import element components

--- src/renderer/sunshine-frame.js
+++ src/renderer/sunshine-frame.js
@@ -1,57 +1,9 @@
 import { createApp } from 'vue'
 import SunshineFrame from './components/SunshineFrame.vue'
-import {
-  ElAffix,
-  ElAlert,
-  ElButton,
-  ElDescriptions,
-  ElDescriptionsItem,
-  ElDialog,
-  ElForm,
-  ElFormItem,
-  ElIcon,
-  ElInput,
-  ElInputNumber,
-  ElLink,
-  ElOption,
-  ElOptionGroup,
-  ElProgress,
-  ElScrollbar,
-  ElSelect,
-  ElSwitch,
-  ElTag,
-  ElTooltip,
-  ElUpload,
-} from 'element-plus'
-import 'element-plus/theme-chalk/base.css'
-import 'element-plus/theme-chalk/el-affix.css'
-import 'element-plus/theme-chalk/el-alert.css'
-import 'element-plus/theme-chalk/el-button.css'
-import 'element-plus/theme-chalk/el-descriptions.css'
-import 'element-plus/theme-chalk/el-descriptions-item.css'
-import 'element-plus/theme-chalk/el-dialog.css'
-import 'element-plus/theme-chalk/el-form.css'
-import 'element-plus/theme-chalk/el-form-item.css'
-import 'element-plus/theme-chalk/el-icon.css'
-import 'element-plus/theme-chalk/el-input.css'
-import 'element-plus/theme-chalk/el-input-number.css'
-import 'element-plus/theme-chalk/el-link.css'
 import 'element-plus/theme-chalk/el-loading.css'
 import 'element-plus/theme-chalk/el-message.css'
 import 'element-plus/theme-chalk/el-message-box.css'
 import 'element-plus/theme-chalk/el-notification.css'
-import 'element-plus/theme-chalk/el-option.css'
-import 'element-plus/theme-chalk/el-option-group.css'
-import 'element-plus/theme-chalk/el-overlay.css'
-import 'element-plus/theme-chalk/el-popper.css'
-import 'element-plus/theme-chalk/el-progress.css'
-import 'element-plus/theme-chalk/el-scrollbar.css'
-import 'element-plus/theme-chalk/el-select.css'
-import 'element-plus/theme-chalk/el-select-dropdown.css'
-import 'element-plus/theme-chalk/el-switch.css'
-import 'element-plus/theme-chalk/el-tag.css'
-import 'element-plus/theme-chalk/el-tooltip.css'
-import 'element-plus/theme-chalk/el-upload.css'
 import './styles/dialog.less'  // 导入对话框样式
 // 导入 Tauri polyfill
 import './tauri-polyfill.js'
@@ -60,33 +12,6 @@ import { i18n, getDefaultLocale, setLocale } from '../i18n/index.js'
 // 初始化语言
 setLocale(getDefaultLocale())
 
-const elementPlusComponents = [
-  ElAffix,
-  ElAlert,
-  ElButton,
-  ElDescriptions,
-  ElDescriptionsItem,
-  ElDialog,
-  ElForm,
-  ElFormItem,
-  ElIcon,
-  ElInput,
-  ElInputNumber,
-  ElLink,
-  ElOption,
-  ElOptionGroup,
-  ElProgress,
-  ElScrollbar,
-  ElSelect,
-  ElSwitch,
-  ElTag,
-  ElTooltip,
-  ElUpload,
-]
-
 const app = createApp(SunshineFrame)
-elementPlusComponents.forEach((component) => {
-  app.component(component.name, component)
-})
 app.use(i18n)
 app.mount('#app')
